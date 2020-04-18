7d1c8d1b19c34e85d148784ca7afbdda8fe3c402
Haoli <haoliyin2002@gmail.com>
2020-04-18 10:56:14 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Navigation from "./Components/Navigation";
-import Home from "./Components/Home";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-      <BrowserRouter basename="/">
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            {/*<Route path="/MusicGenerator" component={MusicGenerator}/>*/}
-            {/*<Route path="/Research" component={Research} exact />*/}
-            {/*<Route path="/AboutUs" component={AboutUs} exact />*/}
-            {/*<Route component={Error}/>*/}
-          </Switch>
-        </div>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
