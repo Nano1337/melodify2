3354c2e2519c5dde355d8a019cabbdf35eedf948
Haoli <haoliyin2002@gmail.com>
2020-04-18 11:58:45 -0500

    fix research page link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./Components/Home";
 import Navigation from "./Components/Navigation";
+import Research from "./Components/Research";
 
 
 function App() {
